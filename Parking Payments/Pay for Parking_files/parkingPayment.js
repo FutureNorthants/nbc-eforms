5a07343ae376f24b81fd4f99fbc1d633809538d6
Luke Barker14 <lbarker@northampton.gov.uk>
2019-09-04 14:06:50 +0100

    changed webservice call from stage to test

--- Parking Payments/Pay for Parking_files/parkingPayment.js
+++ Parking Payments/Pay for Parking_files/parkingPayment.js
@@ -145,7 +145,7 @@
 
 //	------------------------------------------------------------
 	var submitDetails = function () {
-		var url = "https://mycouncil-stage.northampton.digital/CreateParkingPayment";
+		var url = "https://mycouncil-test.northampton.digital/CreateParkingPayment";
 		$.ajaxSetup({
 			beforeSend:function(){
 				$ajaxLoading.show();
