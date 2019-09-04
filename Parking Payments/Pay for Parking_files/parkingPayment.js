fe378a76ea8480958ff9cfd718679d892e4ccc5b
Luke Barker14 <lbarker@northampton.gov.uk>
2019-09-04 11:27:29 +0100

    updated parkingpayment.js to includcorrect link

--- Parking Payments/Pay for Parking_files/parkingPayment.js
+++ Parking Payments/Pay for Parking_files/parkingPayment.js
@@ -145,7 +145,7 @@
 
 //	------------------------------------------------------------
 	var submitDetails = function () {
-		var url = "mycouncil-stage.northampton.digital/CreateParkingPayment";
+		var url = "https://mycouncil-stage.northampton.digital/CreateParkingPayment";
 		$.ajaxSetup({
 			beforeSend:function(){
 				$ajaxLoading.show();
