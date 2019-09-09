c5585df2b2a54be90c05f64bfab485a1d7153718
Luke Barker14 <lbarker@northampton.gov.uk>
2019-09-09 11:28:25 +0100

    Update parkingPayment.js

--- Parking Payments/Pay for Parking_files/parkingPayment.js
+++ Parking Payments/Pay for Parking_files/parkingPayment.js
@@ -57,7 +57,7 @@
 	var $existingPassFields = $(".js-existingPass");
 	var $cboPaymentType = $("#paymentType");
 	var $txtPaymentPeriod = $("#paymentPeriod");
-	var $txtPayPeriodDetails = $("#paymentPeriod")
+	var $txtPayPeriodDetails = $txtPaymentPeriod.options[$txtPaymentPeriod.selectedIndex].text;
 	var $cardPaymentFields = $(".js-card");
 	var $ddPaymentFields = $(".js-dd");
 	var $noTeam = $("#validTeam");
@@ -304,7 +304,7 @@
 		model.addressTown = $town.val();
 		model.addressCounty = $county.val();
 		model.addressPostCode = $postcode.val();
-		model.txtPayPeriodDetails =$txtPayPeriod.options[txtPayPeriod.selectedIndex].text;
+		model.txtPayPeriodDetails =$txtPayPeriodDetails
 		model.txtAddress = model.addressNumber + " " + model.addressStreet + " " + model.addressTown + " " + model.addressCounty + " " + model.addressPostCode;
 		model.txtInternal = internal;
 		
